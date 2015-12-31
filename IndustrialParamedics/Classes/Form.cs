12b68b1ec881ace566299015a7fd79d1a5475540
Erik Johnson <erik.johnson@hybris.com>
2015-12-31 09:24:33 -0800

    Cleaning things up further

--- IndustrialParamedics/Classes/Form.cs
+++ IndustrialParamedics/Classes/Form.cs
@@ -5,6 +5,7 @@ namespace IndustrialParamedics
 {
 	public class Form
 	{
+		
 		Dictionary<string,string> elements = new Dictionary<string,string>();
 		public enum FormType { Activity, CustomerRequest, ServiceRequest};
 		public FormType formType;
